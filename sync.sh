1e659bf4ba6b66beb125515ede920d559b27732b
Felix Geller <fgeller@gmail.com>
2015-10-25 22:42:13 +1300

    Tweaks main page rendering.

--- sync.sh
+++ sync.sh
@@ -1,1 +1,6 @@
-rsync -avz --progress --exclude ".git" index.html style cv i hipp0:~/felix.geller.io/public_html/
+rsync -avz --progress \
+	--exclude ".git" \
+	--exclude "sync.sh" \
+	--exclude "README.org" \
+	* \
+	hipp0:~/felix.geller.io/public_html/
